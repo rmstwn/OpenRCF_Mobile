3fc9f2a5aa95232559d152e3c3095de5d7442396
rmstwn <rmstwn@gmail.com>
2023-02-08 21:06:09 +0900

    Fix Mecanum Kinematics

--- RobotController/OpenRCF/Mobile.cs
+++ RobotController/OpenRCF/Mobile.cs
@@ -13,11 +13,20 @@ using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolTip;
 
 namespace OpenRCF
 {
-    public static class Globals
+    public static class GlobalsMecanum
     {
+        // Mecanum
         public static double Axis1Length = 0.27;
         public static double Axis2Length = 0.25;
-        public static double Diam = 0.1;
+        public static double WheelDiameter = 0.1;
+        public static double Circumference = Math.PI * GlobalsMecanum.WheelDiameter;
+    }
+
+    public static class GlobalsOmnidirectional
+    {
+        // Mecanum
+        public static double AxisLength = 0.16;
+        public static double WheelDiameter = 0.1;
     }
 
     public class Mobile
@@ -53,7 +62,7 @@ namespace OpenRCF
                       ID=13          ID=14
                         |       l2     |
                         |<------------>|
-         */
+        */
 
         static double[] odom = { 0, 0, 0 };
         static double[] cpose = { 0, 0, 0 };
@@ -71,19 +80,23 @@ namespace OpenRCF
 
                 l = l1 + l2
 
-                             |                       |   | w1 |
-                | vx |       |  1    1    1     1    | . | w2 |
-                | vy | = R/4 | -1    1    1    -1    |   | w3 |
+                             |                      |   | w1 |
+                | vx |       |  1    1    1     1   | . | w2 |
+                | vy | = R/4 | -1    1    1    -1   |   | w3 |
                 | wz |       | -1/l  1/l -1/l  1/l  |   | w4 |
             */
 
             double current_time = (int)DateTime.Now.Subtract(new DateTime(2023, 1, 1)).TotalMilliseconds;
 
             //velocities:
-            double move_vel_x = (velocity[0] + velocity[1] + velocity[2] + velocity[3]) * Globals.Diam / 8;
-            double move_vel_y = (velocity[1] - velocity[0] - velocity[3] + velocity[2]) * Globals.Diam / 8;
-            double move_yawrate = (-velocity[0] + velocity[1] - velocity[2] + velocity[3]) * Globals.Diam / 4 / (Globals.Axis1Length + Globals.Axis2Length);
-            
+            //double move_vel_x = (velocity[0] + velocity[1] + velocity[2] + velocity[3]) * GlobalsMecanum.WheelDiameter / 8;
+            //double move_vel_y = (velocity[1] - velocity[0] - velocity[3] + velocity[2]) * GlobalsMecanum.WheelDiameter / 8;
+            //double move_yawrate = (-velocity[0] + velocity[1] - velocity[2] + velocity[3]) * GlobalsMecanum.WheelDiameter / 4 / (GlobalsMecanum.Axis1Length + GlobalsMecanum.Axis2Length);
+
+            double move_vel_x = ((velocity[0] + velocity[1] + velocity[2] + velocity[3]) / 4) / 60 * GlobalsMecanum.Circumference;
+            double move_vel_y = ((velocity[1] - velocity[0] - velocity[3] + velocity[2]) / 4) / 60 * GlobalsMecanum.Circumference;
+            double move_yawrate = ((-velocity[0] + velocity[1] - velocity[2] + velocity[3]) / 4) / 60 * GlobalsMecanum.WheelDiameter / 4 / (GlobalsMecanum.Axis1Length + GlobalsMecanum.Axis2Length);
+
             //positions:
             if (Mecanum.last_time > 0 || Mecanum.last_time < 0)
             {
@@ -142,14 +155,39 @@ namespace OpenRCF
 
             double[] vel = { 0, 0, 0, 0 };
 
-            // w1:
-            vel[0] = 2 / Globals.Diam * (linearX - linearY - (Globals.Axis1Length + Globals.Axis2Length) / 2 * angularZ);
-            // w2:
-            vel[1] = 2 / Globals.Diam * (linearX + linearY + (Globals.Axis1Length + Globals.Axis2Length) / 2 * angularZ);
-            // w3:
-            vel[2] = 2 / Globals.Diam * (linearX + linearY - (Globals.Axis1Length + Globals.Axis2Length) / 2 * angularZ);
-            // w4:
-            vel[3] = 2 / Globals.Diam * (linearX - linearY + (Globals.Axis1Length + Globals.Axis2Length) / 2 * angularZ);
+            //convert m/s to m/min
+            double linear_vel_x_mins = linearX * 60;
+            double linear_vel_y_mins = linearY * 60;
+
+            //convert rad/s to rad/min
+            double angular_vel_z_mins = angularZ * 60;
+
+            //Vt = ω * radius
+            double tangential_vel = angular_vel_z_mins * GlobalsMecanum.Axis1Length;
+
+            double x_rpm = linear_vel_x_mins / GlobalsMecanum.Circumference;
+            double y_rpm = linear_vel_y_mins / GlobalsMecanum.Circumference;
+            double tan_rpm = tangential_vel / GlobalsMecanum.Circumference;
+
+            //calculate for the target motor RPM and direction
+            //front-left motor
+            vel[0] = x_rpm - y_rpm - tan_rpm;
+            //rear-left motor
+            vel[2] = x_rpm + y_rpm - tan_rpm;
+
+            //front-right motor
+            vel[1] = x_rpm + y_rpm + tan_rpm;
+            //rear-right motor
+            vel[3] = x_rpm - y_rpm + tan_rpm;
+
+            //// w1:
+            //vel[0] = 2 / GlobalsMecanum.WheelDiameter * (linearX - linearY - (GlobalsMecanum.Axis1Length + GlobalsMecanum.Axis2Length) / 2 * angularZ);
+            //// w2:
+            //vel[1] = 2 / GlobalsMecanum.WheelDiameter * (linearX + linearY + (GlobalsMecanum.Axis1Length + GlobalsMecanum.Axis2Length) / 2 * angularZ);
+            //// w3:
+            //vel[2] = 2 / GlobalsMecanum.WheelDiameter * (linearX + linearY - (GlobalsMecanum.Axis1Length + GlobalsMecanum.Axis2Length) / 2 * angularZ);
+            //// w4:
+            //vel[3] = 2 / GlobalsMecanum.WheelDiameter * (linearX - linearY + (GlobalsMecanum.Axis1Length + GlobalsMecanum.Axis2Length) / 2 * angularZ);
 
             Joint.Velocity = vel;
 
@@ -187,29 +225,29 @@ namespace OpenRCF
 
             public static void DoWork()
             {
-                Dynamixel.PortOpen("COM3");
+                //Dynamixel.PortOpen("COM3");
 
-                TargetOdom[0] = 0;
+                TargetOdom[0] = 0.151843644923507;
                 TargetOdom[1] = 0;
-                TargetOdom[2] = 5;
+                TargetOdom[2] = 0;
 
                 Move(id, TargetOdom);
             }
 
             public static void DoWork2()
             {
-                Dynamixel.PortOpen("COM3");
+                //Dynamixel.PortOpen("COM3");
 
                 TargetOdom[0] = 0;
                 TargetOdom[1] = 0;
-                TargetOdom[2] = -5;
+                TargetOdom[2] = 0;
 
                 Move(id, TargetOdom);
             }
 
             public static void StopWork()
             {
-                Dynamixel.PortOpen("COM3");
+                //Dynamixel.PortOpen("COM3");
 
                 TargetOdom[0] = 0;
                 TargetOdom[1] = 0;
@@ -236,20 +274,47 @@ namespace OpenRCF
 
                     CurrentVel = Dynamixel.Velocity(DxlId);
 
-                    vel[0] = CurrentVel[0];
-                    vel[1] = CurrentVel[1];
-                    vel[2] = CurrentVel[2];
-                    vel[3] = CurrentVel[3];
+                    vel[0] = TargetVel[0];
+                    vel[1] = TargetVel[1];
+                    vel[2] = TargetVel[2];
+                    vel[3] = TargetVel[3];
+
 
                     Mobile = Mecanum.Mecanum4WForwardKinematics(vel);
 
-                    Console.WriteLine("Velocity:{0}, Velocity:{1}, Velocity:{2}, Velocity:{3}", CurrentVel[0], CurrentVel[1], CurrentVel[2], CurrentVel[3]);
+                    Console.WriteLine("Velocity:{0}, Velocity:{1}, Velocity:{2}, Velocity:{3}", TargetVel[0], TargetVel[1], TargetVel[2], TargetVel[3]);
+                    //Console.WriteLine("Velocity:{0}, Velocity:{1}, Velocity:{2}, Velocity:{3}", CurrentVel[0], CurrentVel[1], CurrentVel[2], CurrentVel[3]);
                     Console.WriteLine("OdomX:{0}, OdomY:{1}, OdomZ:{2}", Mobile.Odometry[0], Mobile.Odometry[1], Mobile.Odometry[2]);
-                    Console.WriteLine("PosX:{0}, PosY:{1}, PosZ:{2}", Mobile.Position[0], Mobile.Position[1], Mobile.Position[2]);
+                    //Console.WriteLine("PosX:{0}, PosY:{1}, PosZ:{2}", Mobile.Position[0], Mobile.Position[1], Mobile.Position[2]);
 
                     Thread.Sleep(10);
                 }
             }
         }
+    }
+
+
+    public class Omnidirectional
+    {
+
+        /*
+           robot:	L: AxisLength
+
+           ID=14  |###|       |###|  ID=11
+                |###|           |###|          y
+              |###| ############# |###|        ^
+                    #############              ¦
+                    #############     front    ¦  
+                    #############              ¦
+              |###| ############# |###|       -¦-----> x
+                |###|           |###|        
+           ID=13  |###|       |###|  ID=12          
+                          |   L   |
+                          |<----->|
+        */
+
+
+
+
     }
 }
