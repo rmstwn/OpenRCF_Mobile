82a0eb371df6d7303fbe7fa1974998db3236b934
rmstwn <rmstwn@gmail.com>
2023-02-13 19:04:30 +0900

    Add Kinematics Omni

--- RobotController/OpenRCF/Mobile.cs
+++ RobotController/OpenRCF/Mobile.cs
@@ -29,6 +29,10 @@ namespace OpenRCF
         public static double WheelDiameter = 0.1;
         public static double Delta = 45; //degree
 
+
+        public static double sin = Math.Sin(GlobalsOmnidirectional.Delta * (Math.PI / 180));
+        public static double cos = Math.Cos(GlobalsOmnidirectional.Delta * (Math.PI / 180));
+
     }
 
     public class Mobile
@@ -88,7 +92,7 @@ namespace OpenRCF
                 | wz |       | -1/l  1/l -1/l  1/l  |   | w4 |
             */
 
-            double current_time = (int)DateTime.Now.Subtract(new DateTime(2023, 1, 1)).TotalMilliseconds;
+            double current_time = DateTime.Now.Subtract(new DateTime(2023, 1, 1)).TotalMilliseconds;
 
             //velocities:
             //double move_vel_x = (velocity[0] + velocity[1] + velocity[2] + velocity[3]) * GlobalsMecanum.WheelDiameter / 8;
@@ -196,7 +200,6 @@ namespace OpenRCF
             //Console.WriteLine("w1: " + velocity[0].ToString() + " w2: " + velocity[1].ToString() + " w3: " + velocity[2].ToString() + " w3: " + velocity[3].ToString());
 
             return Joint;
-
         }
 
         //public class ThreadWork
@@ -334,17 +337,41 @@ namespace OpenRCF
 
         public static MobileInfo Omnidirectional4WForwardKinematics(double[] velocity)
         {
-            double current_time = (int)DateTime.Now.Subtract(new DateTime(2023, 1, 1)).TotalMilliseconds;
+            double current_time = DateTime.Now.Subtract(new DateTime(2023, 1, 1)).TotalMilliseconds;
+            
+            Console.WriteLine(current_time);
 
-            var sin = Math.Sin(GlobalsOmnidirectional.Delta * (Math.PI / 180));
-            var cos = Math.Cos(GlobalsOmnidirectional.Delta * (Math.PI / 180));
-
-            double move_vel_x = velocity[0] * sin - velocity[2] * sin - velocity[1] * cos + velocity[3] * cos;
-            double move_vel_y = -velocity[0] * cos + velocity[2] * cos - velocity[1] * sin + velocity[3] * sin;
+            double move_vel_x = velocity[0] * GlobalsOmnidirectional.sin - velocity[2] * GlobalsOmnidirectional.sin - velocity[1] * GlobalsOmnidirectional.cos + velocity[3] * GlobalsOmnidirectional.cos;
+            double move_vel_y = -velocity[0] * GlobalsOmnidirectional.cos + velocity[2] * GlobalsOmnidirectional.cos - velocity[1] * GlobalsOmnidirectional.sin + velocity[3] * GlobalsOmnidirectional.sin;
             double move_yawrate = -(velocity[0] + velocity[1] + velocity[2] + velocity[3]) / GlobalsOmnidirectional.AxisLength;
 
+            //positions:
+            if (Omnidirectional.last_time > 0 || Omnidirectional.last_time < 0)
+            {
+                dt = (current_time - last_time) / 1000;
 
+                if (dt > 0 && dt < 1)
+                {
+                    // compute second order midpoint velocities
+                    double vel_x_mid = 0.5 * (move_vel_x + last_odom[0]);
+                    double vel_y_mid = 0.5 * (move_vel_y + last_odom[1]);
+                    double yawrate_mid = 0.5 * (move_yawrate + last_odom[2]);
 
+                    // compute midpoint yaw angle
+                    double yaw_mid = cpose[2] + 0.5 * yawrate_mid * dt;
+
+                    // integrate position using midpoint velocities and yaw angle
+                    cpose[0] += vel_x_mid * dt * Math.Cos(yaw_mid) + vel_y_mid * dt * -Math.Sin(yaw_mid);
+                    cpose[1] += vel_x_mid * dt * Math.Sin(yaw_mid) + vel_y_mid * dt * Math.Cos(yaw_mid);
+
+                    // integrate yaw angle using midpoint yawrate
+                    cpose[2] += yawrate_mid * dt;
+                }
+                else
+                {
+                    Console.WriteLine("invalid joint state delta time: " + dt + " sec");
+                }
+            }
 
             odom[0] = move_vel_x;
             odom[1] = move_vel_y;
@@ -357,6 +384,70 @@ namespace OpenRCF
             Mobile.Position = cpose;
 
             return Mobile;
+        }
+
+        public static JointState Omnidirectional4WInverseKinematics(double[] odom)
+        {
+            Matrix J = new Matrix(3, 4);
+            Matrix JJ = new Matrix(3, 3);
+
+            Matrix JTranspose = new Matrix(4, 3);
+            Matrix JInverse = new Matrix(3, 3);
+            Matrix JPseudo = new Matrix(4, 3);
+
+            Vector Odom = new Vector(3);
+            Vector Result = new Vector(4);
+
+            J[0, 0] = (float)GlobalsOmnidirectional.sin;
+            J[0, 1] = -(float)GlobalsOmnidirectional.cos;
+            J[0, 2] = -(float)GlobalsOmnidirectional.sin;
+            J[0, 3] = (float)GlobalsOmnidirectional.cos;
+
+            J[1, 0] = -(float)GlobalsOmnidirectional.cos;
+            J[1, 1] = -(float)GlobalsOmnidirectional.sin;
+            J[1, 2] = (float)GlobalsOmnidirectional.cos;
+            J[1, 3] = (float)GlobalsOmnidirectional.sin;
+
+            J[2, 0] = 1;
+            J[2, 1] = 1;
+            J[2, 2] = 1;
+            J[2, 3] = 1;
+
+            J.ConsoleWrite();
+
+            JTranspose.Set = J.Transpose;
+            JTranspose.ConsoleWrite();
+
+            JJ.Set = J.Times(JTranspose.Get);
+            JJ.ConsoleWrite();
+
+            JInverse.Set = JJ.Inverse;
+            JInverse.ConsoleWrite();
+
+            JPseudo.Set = JTranspose.Times(JInverse.Get);
+            JPseudo.ConsoleWrite();
+
+            odom[0] = 0.4;
+            odom[1] = 0.0;
+            odom[2] = 0.0;
+
+            Odom[0] = (float)odom[0];
+            Odom[1] = (float)odom[1];
+            Odom[2] = (float)odom[2];
+
+            Odom.ConsoleWrite();
+
+            Result.Set = JPseudo.Times(Odom.Get);
+            Result.ConsoleWrite();
+
+            Joint.Velocity[0] = (double)Result[0] / (2 * Math.PI * (GlobalsOmnidirectional.WheelDiameter/2)) * 60;
+            Joint.Velocity[1] = (double)Result[1] / (2 * Math.PI * (GlobalsOmnidirectional.WheelDiameter/2)) * 60;
+            Joint.Velocity[2] = (double)Result[2] / (2 * Math.PI * (GlobalsOmnidirectional.WheelDiameter/2)) * 60;
+            Joint.Velocity[3] = (double)Result[3] / (2 * Math.PI * (GlobalsOmnidirectional.WheelDiameter/2)) * 60;
+
+            Console.WriteLine(Joint.Velocity);
+
+            return Joint;
         }
 
         public class ThreadWork
@@ -394,17 +485,25 @@ namespace OpenRCF
                 //TargetOdom[2] = 0;
 
                 //Move(id, TargetOdom);
+                //for (; ; )
+                //{
+                //    vel[0] = 0.141421356237310;
+                //    vel[1] = -0.141421356237310;
+                //    vel[2] = -0.141421356237310;
+                //    vel[3] = 0.141421356237310;
 
-                vel[0] = 0.141421356237310;
-                vel[1] = -0.141421356237310;
-                vel[2] = -0.141421356237310;
-                vel[3] = 0.141421356237310;
+                //    Mobile = Omnidirectional4WForwardKinematics(vel);
 
-                Mobile = Omnidirectional4WForwardKinematics(vel);
+                //    Console.WriteLine("Velocity:{0}, Velocity:{1}, Velocity:{2}, Velocity:{3}", vel[0], vel[1], vel[2], vel[3]);
+                //    //Console.WriteLine("Velocity:{0}, Velocity:{1}, Velocity:{2}, Velocity:{3}", CurrentVel[0], CurrentVel[1], CurrentVel[2], CurrentVel[3]);
+                //    Console.WriteLine("OdomX:{0}, OdomY:{1}, OdomZ:{2}", Mobile.Odometry[0], Mobile.Odometry[1], Mobile.Odometry[2]);
+                //    Console.WriteLine("PosX:{0}, PosY:{1}, PosZ:{2}", Mobile.Position[0], Mobile.Position[1], Mobile.Position[2]);
 
-                Console.WriteLine("OdomX:{0}, OdomY:{1}, OdomZ:{2}", Mobile.Odometry[0], Mobile.Odometry[1], Mobile.Odometry[2]);
+                //    Thread.Sleep(10);
+                //}
 
-                Thread.Sleep(10);
+                Joint = Omnidirectional4WInverseKinematics(TargetOdom);
+
             }
 
             public static void DoWork2()
